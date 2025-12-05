63b2d47ba1fa500ceecf5bc9cc4f53f9a7b75a5b
Mafizik <165509365+Mafizik@users.noreply.github.com>
2025-12-06 01:25:05 +0300

    Add files via upload

---
+++ plr_mimgui_hotkeys.lua
@@ -0,0 +1,217 @@
+--[[
+	   
+	Author: СоМиК (модифицировано для поддержки XButton 1 и 2)
+	Links:
+		- https://www.blast.hk/members/406277/
+		- https://t.me/klamet_one
+		- https://vk.com/klamet1
+
+]]
+
+local imgui = require 'mimgui'
+local vk = require 'vkeys'
+
+HOTKEY = {
+	MODULEINFO = {
+		version = 1,
+		author = 'СоМиК and AI'
+	},
+	Text = {
+		WaitForKey = 'Нажмите любую клавишу...',
+		NoKey = '< Свободно >'
+	},
+	List = {},
+	ActiveKeys = {},
+	ReturnHotKeys = nil,
+	HotKeyIsEdit = nil,
+	CancelKey = 0x1B,
+	RemoveKey = 0x08,
+	True = true
+}
+
+local WM_XBUTTONDOWN = 0x020B
+local WM_XBUTTONUP = 0x020C
+local MK_XBUTTON1 = 0x0020
+local MK_XBUTTON2 = 0x0040
+local VK_XBUTTON1 = 0x05
+local VK_XBUTTON2 = 0x06
+
+local function get_xbutton_id(lparam)
+    if bit.band(lparam, MK_XBUTTON1) ~= 0 then 
+        return VK_XBUTTON1
+    elseif bit.band(lparam, MK_XBUTTON2) ~= 0 then 
+        return VK_XBUTTON2
+    end
+    return 0
+end
+
+deepcopy = function(orig)
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		copy = {}
+		for orig_key, orig_value in next, orig, nil do
+			copy[deepcopy(orig_key)] = deepcopy(orig_value)
+		end
+		setmetatable(copy, deepcopy(getmetatable(orig)))
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
+
+
+local getKeysText = function(name)
+	local keysText = {}
+	if HOTKEY.List[name] ~= nil then
+		for k, v in ipairs(HOTKEY.List[name].keys) do
+			table.insert(keysText, vk.id_to_name(v)) 
+		end
+	end
+	return table.concat(keysText, ' + ')
+end
+
+local searchHotKey = function(keys)
+	local needCombo = deepcopy(keys)
+	table.sort(needCombo)
+	needCombo = table.concat(needCombo, ':')
+	for k, v in pairs(HOTKEY.List) do
+		if next(v.keys) then
+			local foundCombo = deepcopy(v.keys)
+			table.sort(foundCombo)
+			foundCombo = table.concat(foundCombo, ':')
+			if foundCombo == needCombo then
+				v.callback()
+				break
+			end
+		end
+	end
+end
+
+HOTKEY.RegisterHotKey = function(name, soloKey, keys, callback)
+	if HOTKEY.List[name] == nil then
+		HOTKEY.List[name] = {
+			soloKey = soloKey,
+			keys = keys,
+			callback = callback
+		}
+		return {
+			name,
+			['ShowHotKey'] = setmetatable({}, {__call = function(self, arg1, arg2) return HOTKEY.ShowHotKey(arg1[1], arg2) end}),
+			['EditHotKey'] = setmetatable({}, {__call = function(self, arg1, arg2) return HOTKEY.EditHotKey(arg1[1], arg2) end}),
+			['RemoveHotKey'] = setmetatable({}, {__call = function(self, arg) return HOTKEY.RemoveHotKey(arg[1]) end}),
+			['GetHotKey'] = setmetatable({}, {__call = function(self, arg) return HOTKEY.GetHotKey(arg[1]) end})
+		}
+	end
+end
+
+HOTKEY.EditHotKey = setmetatable(
+	{},
+	{
+		__call = function(self, name, keys)
+			if HOTKEY.List[name] ~= nil then
+				HOTKEY.List[name].keys = keys
+				return true
+			end
+			return false
+		end
+	}
+)
+
+HOTKEY.RemoveHotKey = setmetatable(
+	{},
+	{
+		__call = function(self, name)
+			HOTKEY.List[name] = nil
+			return true
+		end
+	}
+)
+
+HOTKEY.ShowHotKey = setmetatable(
+	{},
+	{
+		__call = function(self, name, sizeButton)
+			if HOTKEY.List[name] ~= nil then
+				local HotKeyText = #HOTKEY.List[name].keys == 0 and ((HOTKEY.HotKeyIsEdit ~= nil and HOTKEY.HotKeyIsEdit.NameHotKey == name) and HOTKEY.Text.WaitForKey or HOTKEY.Text.NoKey) or getKeysText(name)
+				if imgui.Button(('%s##HK:%s'):format(HotKeyText, name), sizeButton) then
+					HOTKEY.HotKeyIsEdit = {
+						NameHotKey = name,
+						BackupHotKeyKeys = HOTKEY.List[name].keys,
+					}
+					HOTKEY.ActiveKeys = {}
+					HOTKEY.HotKeyIsEdit.ActiveKeys = {}
+					HOTKEY.List[name].keys = {}
+				end
+				if HOTKEY.ReturnHotKeys == name then
+					HOTKEY.ReturnHotKeys = nil
+					return true
+				end
+			else
+				imgui.Button('Хоткей не найден', sizeButton)
+			end
+		end
+	}
+)
+
+HOTKEY.GetHotKey = setmetatable(
+	{},
+	{
+		__call = function(self, name)
+			if HOTKEY.List[name] ~= nil then
+				return HOTKEY.List[name].keys
+			end
+		end
+	}
+)
+
+addEventHandler('onWindowMessage', function(msg, key, lparam)
+    local processedKey = key
+    if msg == WM_XBUTTONDOWN or msg == WM_XBUTTONUP then
+        processedKey = get_xbutton_id(key) 
+    end
+
+    if msg == 0x100 or msg == 260 or msg == WM_XBUTTONDOWN then 
+		if HOTKEY.HotKeyIsEdit == nil then
+			if processedKey ~= HOTKEY.CancelKey and processedKey ~= HOTKEY.RemoveKey and processedKey ~= 0x1B and processedKey ~= 0x08 and next(HOTKEY.List) then
+				local found = false
+				for k, v in ipairs(HOTKEY.ActiveKeys) do
+					if v == processedKey then
+						found = true
+						break
+					end
+				end
+				if not found then
+					table.insert(HOTKEY.ActiveKeys, processedKey)
+					searchHotKey(HOTKEY.ActiveKeys)
+					table.remove(HOTKEY.ActiveKeys)
+				end
+			end
+		else 
+			if processedKey == HOTKEY.CancelKey then
+				HOTKEY.List[HOTKEY.HotKeyIsEdit.NameHotKey].keys = HOTKEY.HotKeyIsEdit.BackupHotKeyKeys
+				HOTKEY.HotKeyIsEdit = nil
+			elseif processedKey == HOTKEY.RemoveKey then
+				HOTKEY.List[HOTKEY.HotKeyIsEdit.NameHotKey].keys = {}
+				HOTKEY.ReturnHotKeys = HOTKEY.HotKeyIsEdit.NameHotKey
+				HOTKEY.HotKeyIsEdit = nil
+			elseif processedKey ~= 0x1B and processedKey ~= 0x08 then
+				local found = false
+				for k, v in ipairs(HOTKEY.HotKeyIsEdit.ActiveKeys) do
+					if v == processedKey then
+						found = true
+						break
+					end
+				end
+				if not found then
+					table.insert(HOTKEY.List[HOTKEY.HotKeyIsEdit.NameHotKey].keys, processedKey)
+					HOTKEY.ReturnHotKeys = HOTKEY.HotKeyIsEdit.NameHotKey
+					HOTKEY.HotKeyIsEdit = nil
+				end
+			end
+			consumeWindowMessage(true, true)
+		end
+	end
+end)
+
+return HOTKEY
